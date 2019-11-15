83ddeb11f448cdf7b1d704b59c1b60d866d2a3ad
Franz Knipp <franz@qnipp.com>
2019-11-15 15:50:52 +0100

    Add a static asset to the output

--- client/main.js
+++ client/main.js
@@ -1,1 +1,7 @@
 console.log(`Greetings from ${module.id}!`);
+
+import AustrianFlag from "famfamfam-flags/dist/png/at.png";
+
+console.log(`The flag is ${AustrianFlag}`);
+
+document.getElementById('client-render-target').innerHTML=`Browser time: ${new Date} <img src="${AustrianFlag}"/>`;
--- server/main.js
+++ server/main.js
@@ -1,15 +1,18 @@
 import { Meteor } from "meteor/meteor";
 import { onPageLoad } from "meteor/server-render";
 
+import AustrianFlag from "famfamfam-flags/dist/png/at.png";
+
 Meteor.startup(() => {
   // Code to run on server startup.
   console.log(`Greetings from ${module.id}!`);
+  console.log(`The flag is ${AustrianFlag}`)
 });
 
 onPageLoad(sink => {
   // Code to run on every request.
   sink.renderIntoElementById(
     "server-render-target",
-    `Server time: ${new Date}`
+    `Server time: ${new Date} <img src="${AustrianFlag}"/>`
   );
 });
